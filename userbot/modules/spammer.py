c30418603b858865b19c50eb0574d6032273b134
Orkun <orkuncoskun9@gmail.com>
2019-06-07 19:12:51 +0300

    Update spammer.py

--- userbot/modules/spammer.py
+++ userbot/modules/spammer.py
@@ -43,8 +43,6 @@ async def spammer(e):
         for i in range(1, counter):
             await e.respond(spam_message)
             time.sleep(0.7)
-            if pattern="^.slowspamstop":
-                break
         await e.delete()
         if LOGGER:
             await e.client.send_message(
